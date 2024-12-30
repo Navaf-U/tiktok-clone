339ceab358d02832defaf1ee23530e8911e13b77
navaf <navafsoft@gmail.com>
2024-12-30 15:29:09 +0530

    feat (manageErr middleware) : added manageError middleware fix (authController,userSchema) : minor updates that changed name to username and user can login either username or with email

--- server/controllers/auth/authController.js
+++ server/controllers/auth/authController.js
@@ -1,23 +1,34 @@
 import User from "../../models/userSchema.js";
 import CustomError from "../../util/CustomError.js";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const createToken = (id, role, expiresIn) => {
-  return jwt.sign({ id }, { role }, process.env.JWT_SECRET, { expiresIn });
+  return jwt.sign({ id ,role }, process.env.JWT_TOKEN, { expiresIn });
 };
 const registerUser = async (req, res, next) => {
-  const { email, password } = req.body;
-  const existingUser = await User.findOne({ email });
+  const {  username,email, password } = req.body;
+  const existingUser = await User.findOne({
+    $or: [{ email }, { username }],
+  });
+
   if (existingUser) {
-    return next(new CustomError("User already exists", 400));
+    if (existingUser.email === email) {
+      return next(new CustomError("Email already exists", 400));
+    }
+    if (existingUser.username === username) {
+      return next(new CustomError("Username already exists", 400));
+    }
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  await User.create({ email, password: hashPassword });
+  await User.create({ username , email, password: hashPassword });
   res.json({ message: "User Created successfully" });
 };
 
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+const loginUser = async (req, res,next) => {
+  const { emailOrUsername, password } = req.body;
+  const user = await User.findOne({
+    $or: [{ email: emailOrUsername }, { username: emailOrUsername }],
+  });
   if (!user) {
     return next(new CustomError("User does not exist", 400));
   }
@@ -26,7 +37,13 @@ const loginUser = async (req, res) => {
     return next(new CustomError("Invalid credentials", 400));
   }
   const token = createToken(user._id, user.role, "1h");
-  res.json({ message: "Login successful" ,token});
+  const currUser = {
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    role: user.role,
+  };
+  res.json({ message: "Login successful", token , user: currUser });
 };
 
-export { registerUser ,loginUser};
+export { registerUser, loginUser };
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import ConnectDataBase from "./config/ConnectDataBase.js";
 import dotenv from "dotenv";
 import authRoutes from './routes/authRoutes.js'
-// import manageError from './middlewares/manageError.js';
+import manageError from "./middlewares/ManageError.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -16,7 +16,7 @@ app.use('/auth',authRoutes)
 app.all("*",(req,res)=>{
   res.status(400).json({message:'cannot access the endpoint'})
 })
-// app.use(manageError())
+app.use(manageError)
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
