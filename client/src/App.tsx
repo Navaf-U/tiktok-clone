6ae6faba9ad910b3227edb9b1b9dd630b4a3ffc9
navaf <navafsoft@gmail.com>
2024-12-30 11:33:00 +0530

    feat (navbar) : created navbar and installed lot of libraries

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <>
-    <h1>hi</h1>
+    <NavBar/>
+    <h1 className='text-red-500'>hi</h1>
     </>
   )
 }
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,32 @@
+import tiktokFullPng from "../assets/tiktok-full-icon.png";
+import tiktokIcon from "../assets/tiktok-icon.png"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { BiMessageMinus } from "react-icons/bi";
+import { FaPlus } from "react-icons/fa";
+import { FaRegUserCircle } from "react-icons/fa";
+
+function NavBar() {
+  return (
+    <div className="flex items-center justify-between bg-[#121212] w-full">
+      <div className="flex items-center w-full justify-between my-2 my-lg-0">
+            <img src={tiktokFullPng} className="ms-3 w-32 h-[35px]" alt="" />
+            <Input
+              type="text"
+              placeholder="Search"
+              className="rounded-full h-[45px] text-[#c9c9c9] placeholder:text-[#c9c9c9] placeholder:text-[16px] font-medium bg-[#2e2e2e] ms-2 w-[500px] border-0"
+            />
+            <div className="flex w-[25%] justify-between items-center ms-2 me-4">
+            <Button variant={"grays"}>
+              <FaPlus /> Upload 
+            </Button>
+            <BiMessageMinus className="text-white" size={30} />
+            <Button variant={'grays'} className="bg-[#121212] rounded-none border border-[#ffffff81]"> <img src={tiktokIcon} className="w-5 outline-1 outline-white" alt="" /> Get Coin</Button>
+            <FaRegUserCircle size={30} className="text-white"/>
+            </div>
+            </div>
+    </div>
+  )
+}
+
+export default NavBar
