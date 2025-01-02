5d64ea43d3b5f275dd1a55736ed4bd41569a3278
navaf <navafsoft@gmail.com>
2025-01-03 00:27:30 +0530

    feat (userDetailsEdit , UserProfile , axiosIns) : created userDetails changing modal and user can change their bio and it will change in real time , and updated userProfilePage and axiosInstance

--- client/src/Pages/UserProfile.tsx
+++ client/src/Pages/UserProfile.tsx
@@ -7,20 +7,26 @@ import { IoSettingsOutline } from "react-icons/io5";
 import { RiShareForwardLine } from "react-icons/ri";
 import { Button } from "@/components/ui/button";
 import { HiDotsHorizontal } from "react-icons/hi";
+import UserDetailsEdit from "@/modal/UserDetailsEdit";
+import HomeSidebar from "@/components/HomeSideBar";
 interface User {
   id: string;
   username: string;
   email: string;
-  password: string;
+  profile: string;
+  bio: string;
   role: string;
 }
 
 function UserProfile(): JSX.Element {
   const userContext = useContext(UserContext);
-  const { currUser } = userContext || {};
+  if (!userContext) {
+    throw new Error("UserContext is not available");
+  }
+  const { currUser, showUserEdit, setShowUserEdit } = userContext;
   const { username } = useParams();
   const [otherUser, setOtherUser] = useState<User | null>(null);
-  const [userNotFound, setUserNotFound] = useState <boolean>(false);
+  const [userNotFound, setUserNotFound] = useState<boolean>(false);
   const isCurrUser = currUser?.username === username;
 
   useEffect(() => {
@@ -31,7 +37,7 @@ function UserProfile(): JSX.Element {
         );
         const data = res.data;
         if (data.username === username) {
-          setOtherUser(data);
+          setOtherUser(data); 
           setUserNotFound(false);
         }
       } catch (error) {
@@ -43,17 +49,21 @@ function UserProfile(): JSX.Element {
       }
     };
     FetchUser();
-  }, [username]);
+  }, [username,currUser]);
 
   return (
     <div>
       {isCurrUser && (
-        <div className="flex mt-20 ms-72">
+        <div className="flex mt-24 ms-64">
           <FaUserCircle className="h-[200px] w-[200px] text-white" />
           <div>
             <h1 className="text-white text-2xl ms-5 mt-8">{username}</h1>
             <div className="flex">
-              <Button variant={"pinks"} className="ms-4 mt-3 w-[115px] h-[40px]">
+              <Button
+                onClick={() => setShowUserEdit(true)}
+                variant={"pinks"}
+                className="ms-4 mt-3 w-[115px] h-[40px]"
+              >
                 Edit Profile
               </Button>
               <IoSettingsOutline className="ms-3 mt-3 w-[40px] p-2 h-[40px] bg-[#303030] rounded-md text-white hover:bg-[#3e3e3e] cursor-pointer" />
@@ -64,19 +74,31 @@ function UserProfile(): JSX.Element {
               <p className="hover:underline cursor-pointer">0 Followers</p>
               <p className="hover:underline cursor-pointer">0 Likes</p>
             </div>
+            <div className="mt-2 ms-5 font-normal text-[17px]">
+              {isCurrUser && currUser?.bio ? (
+                <h1>{currUser.bio}</h1>
+              ) : (
+                <p>No bio yet.</p>
+              )}
+            </div>
           </div>
         </div>
       )}
       {otherUser && !isCurrUser && (
-        <div className="flex mt-20 ms-72">
+        <div className="flex mt-24 ms-72">
           <FaUserCircle className="h-[200px] w-[200px] text-white" />
           <div>
             <h1 className="text-white text-2xl ms-5 mt-8">{username}</h1>
             <div className="flex">
-              <Button variant={"pinks"} className="ms-4 mt-3 w-[115px] h-[40px]">
+              <Button
+                variant={"pinks"}
+                className="ms-4 mt-3 w-[115px] h-[40px]"
+              >
                 Follow
               </Button>
-              <button className="ms-4 mt-[12px] bg-[#303030] rounded-md w-[115px] h-[40px] text-white hover:bg-[#3e3e3e] cursor-pointer">Message</button>
+              <button className="ms-4 mt-[12px] bg-[#303030] rounded-md w-[115px] h-[40px] text-white hover:bg-[#3e3e3e] cursor-pointer">
+                Message
+              </button>
               <RiShareForwardLine className="ms-3 mt-3 w-[40px] p-2 h-[40px] bg-[#303030] rounded-md text-white hover:bg-[#3e3e3e] cursor-pointer" />
               <HiDotsHorizontal className="ms-3 mt-3 w-[40px] p-2 h-[40px] bg-[#303030] rounded-md text-white hover:bg-[#3e3e3e] cursor-pointer" />
             </div>
@@ -85,15 +107,26 @@ function UserProfile(): JSX.Element {
               <p className="hover:underline cursor-pointer">0 Followers</p>
               <p className="hover:underline cursor-pointer">0 Likes</p>
             </div>
+            <div className="mt-2 ms-5 font-normal text-[17px]">
+              {otherUser && otherUser?.bio ? (
+                <h1>{otherUser.bio}</h1>
+              ) : (
+                <p>No bio yet.</p>
+              )}
+            </div>
           </div>
         </div>
       )}
       {userNotFound && (
         <div className="flex flex-col justify-center items-center mt-20">
           <h1 className="text-white text-3xl font-bold">User Not Found</h1>
-          <p className="text-gray-400 mt-2">The user you are looking for does not exist.</p>
+          <p className="text-gray-400 mt-2">
+            The user you are looking for does not exist.
+          </p>
         </div>
       )}
+      <HomeSidebar />
+      {showUserEdit && <UserDetailsEdit />}
     </div>
   );
 }
---
+++ client/src/modal/UserDetailsEdit.tsx
@@ -0,0 +1,99 @@
+import { Button } from "@/components/ui/button";
+import { UserContext } from "@/context/UserProvider";
+import { useContext, useState } from "react";
+import { IoIosClose } from "react-icons/io";
+import axiosInstance from "../utilities/axiosInstance";
+function UserDetailsEdit(): JSX.Element {
+  const userContext = useContext(UserContext);
+  if (!userContext) {
+    throw new Error("UserContext is not available");
+  }
+  const { setShowUserEdit, currUser,setCurrUser } = userContext || {};
+  const [bio, setBio] = useState("");
+  const maxLength = 80;
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (e.target.value.length <= maxLength) {
+      setBio(e.target.value);
+    }
+  };
+
+  const handleSave : () => Promise<void> = async () => {
+    const {data} = await axiosInstance.patch("/user/profile/update", {
+      bio: bio,
+    });
+    setCurrUser(data);
+    localStorage.setItem("currUser", JSON.stringify(currUser));
+    setShowUserEdit(false);
+  };
+
+
+
+  return (
+    <div className="fixed inset-0 bg-[#191919] rounded-md flex flex-col left-[25%] right-[25%] w-[50%] z-50">
+      <div className="flex justify-between items-center mt-5 mb-3 mx-9">
+        <h1 className="font-medium text-[#e8e8e8] text-[22px]">Edit profile</h1>
+        <IoIosClose
+          onClick={() => setShowUserEdit(false)}
+          className="cursor-pointer"
+          size={40}
+        />
+      </div>
+      <hr className="w-full opacity-45" />
+      <div className="flex justify-between py-5 items-start mt-5 mb-3 mx-10">
+        <h1 className="font-medium text-[#e8e8e8]  text-[20px]">
+          Profile photo
+        </h1>
+        <input type="file" className="" />
+      </div>
+      <hr className="opacity-45 ms-5 w-[95%]" />
+      <div className="flex justify-between gap-4 py-3 items-start mt-5 mb-3 mx-10">
+        <h1 className="font-medium text-[#e8e8e8] text-[20px]">Username</h1>
+        <div>
+          <h1 className="text-center font-medium text-[#e8e8e8] text-[17px]">
+            {currUser?.username}
+          </h1>
+          <p className="text-center text-[10px] text-[#ffffffbc]">
+            www.tiktokClone.com/@{currUser?.username}
+            <br />
+            Your username can be changed once every 30 days. You can change it
+            again after Jan 27, 2025.
+          </p>
+        </div>
+      </div>
+      <hr className="opacity-45 ms-5 w-[95%] py-3" />
+      <div className="flex justify-between items-start mt-5 mb-3 mx-10">
+        <h1 className="font-medium text-[#e8e8e8]  text-[20px]">Bio</h1>
+        <div className="flex flex-col">
+          <textarea
+            name="bio"
+            id="bio"
+            value={bio}
+            placeholder="bio"
+            onChange={handleChange}
+            className="bg-[#2e2e2e] w-[360px] h-[100px] rounded-md p-2"
+          ></textarea>
+          <div className="text-sm text-gray-400 mt-2">
+            {bio.length}/{maxLength} characters
+          </div>
+        </div>
+      </div>
+      <hr className="opacity-25 mt-2 w-[100%]" />
+      <div className="flex gap-4 justify-end me-10">
+        <Button
+          variant={"grays"}
+          className="w-24 mt-8 h-9 rounded-sm"
+          onClick={() => setShowUserEdit(false)}
+        >
+          {" "}
+          Cancel
+        </Button>
+        <Button variant={"pinks"} className="w-24 mt-8 h-9 rounded-sm" onClick={handleSave}>
+          {" "}
+          Save
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default UserDetailsEdit;
