dd9d727f565af821b1f037cdbb8250194a0d1545
navaf <navafsoft@gmail.com>
2025-01-03 17:12:31 +0530

    fix (userController) : fixed a minor bug in userUpdate controller where the cloudinary cant destroy the empty string

--- server/controllers/user/userController.js
+++ server/controllers/user/userController.js
@@ -1,5 +1,5 @@
 import User from "../../models/userSchema.js";
-import cloudinary from '../../config/CloudinaryConfig.js'
+import cloudinary from "../../config/CloudinaryConfig.js";
 import CustomError from "../../util/CustomError.js";
 const getOneUser = async (req, res) => {
   const { username } = req.params;
@@ -11,22 +11,21 @@ const getOneUser = async (req, res) => {
 };
 
 const userUpdate = async (req, res, next) => {
-    const { bio } = req.body;
-    const user = await User.findOne({ _id: req.user.id }, { password: 0 });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+  const { bio } = req.body;
+  const user = await User.findOne({ _id: req.user.id }, { password: 0 });
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  if (req.file) {
+    if (user.profile) {
+      await cloudinary.uploader.destroy(user.profile);
     }
-    if (req.file) {
-      // await cloudinary.uploader.destroy(user.profile);
-      console.log(req.uploadedFile ,"secure_url");
-      console.log("hi from userUpdate inside reqFIle");
-      user.profile = req.uploadedFile.secure_url;
-      console.log(user.profile)
-    }
-    if (bio !== undefined) {
-      user.bio = bio;
-    }
-    await user.save();
-    res.json(user);
+    user.profile = req.uploadedFile.secure_url;
+  }
+  if (bio !== undefined) {
+    user.bio = bio;
+  }
+  await user.save();
+  res.json(user);
 };
 export { getOneUser, userUpdate };
