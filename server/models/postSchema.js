0594de01e3f1a3ae85c67a40b87615b31f0820b7
navaf <navafsoft@gmail.com>
2025-01-05 15:36:54 +0530

    feat (postSchema) : created post schema and exported :>

---
+++ server/models/postSchema.js
@@ -0,0 +1,14 @@
+import mongoose from mongoose
+
+const postSchema = mongoose.Schema({
+    username:{type: String, required: true},
+    description:{type: String, required: false},
+    file:{type: String, required: true},
+    date:{type: Date, default: Date.now},
+    likes:{type: [String], default: []},
+    comments:{type: [String],default: []},
+    favorites:{type: [String],default: []},
+})
+
+const Posts = mongoose.model("Posts", postSchema)
+export default Posts
