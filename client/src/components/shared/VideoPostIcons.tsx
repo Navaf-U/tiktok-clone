949abce895bcab476b5c8b98114e41de328cba99
navaf <navafsoft@gmail.com>
2025-01-06 21:54:15 +0530

    feat (index.html, VideoPostIcons) : update favicon and title, created and enhanced VideoPostIcons layout for better alignment and it is reusable component

--- client/src/components/shared/VideoPostIcons.tsx
+++ client/src/components/shared/VideoPostIcons.tsx
@@ -4,23 +4,15 @@ import { FaBookmark } from "react-icons/fa6";
 import { PiShareFatFill } from "react-icons/pi";
 function VideoPostIcons(): JSX.Element {
     return (
-      <div className="flex flex-col"> 
-        <div>
+      <div className="text-center flex flex-col gap-1 justify-center items-center"> 
         <IoHeart/>
         <p>1000</p>
-        </div>
-        <div>
         <FaRegCommentDots/>
         <p>1000</p>
-        </div>
-        <div>
         <FaBookmark/>
         <p>1000</p>
-        </div>
-        <div>
         <PiShareFatFill/>
         <p>1000</p>
-        </div>
       </div>
     )
   }
