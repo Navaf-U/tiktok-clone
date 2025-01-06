dc9ee2f53de7aefc7199483b312adde76f73a9cc
navaf <navafsoft@gmail.com>
2025-01-06 17:34:28 +0530

    feat (postsController, userRoutes) : add video description update and delete functionality, enhance video upload response

--- server/controllers/user/postsController.js
+++ server/controllers/user/postsController.js
@@ -1,29 +1,48 @@
+import cloudinary from "../../config/CloudinaryConfig.js";
 import Posts from "../../models/postsSchema.js";
 import User from "../../models/userSchema.js";
 const userVideoPost = async (req, res) => {
-  const { description } = req.body;
   const uploadedFile = req.uploadedFile;
   const userID = req.user.id;
-  console.log("Request body:", req.body);
-  console.log("Uploaded file:", req.uploadedFile);
-
-  if (!description || !uploadedFile) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  if (uploadedFile.format !== "mp4" || uploadedFile.resource_type !== "video") {
-    return res.status(400).json({ message: "Invalid file type" });
+  if (uploadedFile) {
+    if (uploadedFile.format !== "mp4" || uploadedFile.resource_type !== "video") {
+      return res.status(400).json({ message: "Invalid file type" });
+    }
   }
   const user = await User.findById(userID);
   if (!user || !user.username) {
     return res.status(400).json({ message: "User information is missing" });
   }
-  const file = req.uploadedFile.secure_url;
+  const file = req.uploadedFile?.secure_url;
+  const fileSize = req.uploadedFile?.bytes / (1024 * 1024);
+  const fileDuration = req.uploadedFile?.duration;
   const post = await Posts.create({
     username: user.username,
-    description,
     file,
   });
-  res.json(post);
+  res.json({_id : post._id,file,fileSize,fileDuration,message:"video uploaded successfully"});
 };
 
-export {userVideoPost};
+const userVideoDescription = async (req, res) => {
+  const { description } = req.body;
+  const postID = req.params.id;
+  const post = await Posts.findById(postID);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  post.description = description;
+  await post.save();
+};
+
+const userDeleteVideo = async (req, res) => {
+  const postID = req.params.id;
+  const post = await Posts.findById(postID);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  await cloudinary.uploader.destroy(post.file);
+  await Posts.deleteOne({ _id: postID });
+  res.json({ message: "Post deleted successfully" });
+};
+
+export {userVideoPost,userVideoDescription,userDeleteVideo};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,11 +4,13 @@ import { getOneUser, userUpdate } from "../controllers/user/userController.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 import upload from "../config/MulterConfig.js";
 import { uploadToCloudinary } from "../middlewares/fileUpload.js";
-import { userVideoPost } from "../controllers/user/postsController.js";
+import { userDeleteVideo, userVideoDescription, userVideoPost } from "../controllers/user/postsController.js";
 const Router = express.Router();
 Router
 .get("/profile/:username", tryCatch(getOneUser))
 .patch("/profile/update",upload.single("file"),verifyToken,uploadToCloudinary,tryCatch(userUpdate))
 .post("/posts/video",upload.single("file"),verifyToken,uploadToCloudinary,tryCatch(userVideoPost))
+.post("/posts/description/:id",verifyToken,tryCatch(userVideoDescription))
+.delete("/posts/delete/:id",verifyToken,tryCatch(userDeleteVideo))
 
 export default Router;
