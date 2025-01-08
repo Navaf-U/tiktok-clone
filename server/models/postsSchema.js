58172cf7eb8657e2d4ed5f05631f2c21a8deb8e6
navaf <navafsoft@gmail.com>
2025-01-08 16:58:35 +0530

    feat (postsSchema): update comments structure to include user reference, text, and timestamp

--- server/models/postsSchema.js
+++ server/models/postsSchema.js
@@ -6,7 +6,13 @@ const postsSchema = mongoose.Schema({
     file:{type: String, required: true},
     date:{type: Date, default: Date.now},
     likes:{type: [String], default: []},
-    comments:{type: [String],default: []},
+    comments: [
+        {
+          user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+          text: { type: String, required: true },                     
+          createdAt: { type: Date, default: Date.now },               
+        },
+      ],
     favorites:{type: [String],default: []},
 })
 
