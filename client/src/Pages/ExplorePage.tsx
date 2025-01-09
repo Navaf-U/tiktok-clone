df268c0f7291c74f65a623b0e71ebd745000e6c4
navaf <navafsoft@gmail.com>
2025-01-09 14:35:09 +0530

    feat (ExplorePage): create ExplorePage component to display user posts with navigation and sidebar

---
+++ client/src/Pages/ExplorePage.tsx
@@ -0,0 +1,33 @@
+import NavBar from "@/components/NavBar";
+import VideoCard from "@/components/shared/VideoCard";
+import HomeSidebar from "@/components/sidebars/HomeSideBar";
+import { UserContext } from "@/context/UserProvider";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+function ExplorePage(): JSX.Element {
+  const userContext = useContext(UserContext);
+  const posts = userContext?.posts || [];
+
+  return (
+    <div>
+      <NavBar />
+      <div className="flex">
+        <div className="w-1/5  hidden md:block">
+          <HomeSidebar />
+        </div>
+        <div className="flex-grow mt-20 ms-8 pt-4">
+          <div className="flex flex-wrap gap-6 justify-start">
+            {posts.map((post: { _id: string; file: string }) => (
+              <Link to={`/user/video/${post._id}`} key={post._id}>
+                <VideoCard file={post.file} />
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExplorePage;
