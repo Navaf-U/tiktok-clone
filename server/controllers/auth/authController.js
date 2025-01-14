a7343ead0b73ff134486dc41738ce858b636e152
navaf <navafsoft@gmail.com>
2025-01-14 17:43:17 +0530

    feat (authController): implement token refresh logic with error handling and cookie management

--- server/controllers/auth/authController.js
+++ server/controllers/auth/authController.js
@@ -3,10 +3,11 @@ import CustomError from "../../util/CustomError.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 const createToken = (id, role, expiresIn) => {
-  return jwt.sign({ id ,role }, process.env.JWT_TOKEN, { expiresIn });
+  return jwt.sign({ id, role }, process.env.JWT_TOKEN, { expiresIn });
 };
+
 const registerUser = async (req, res, next) => {
-  const {  username,email, password , dob } = req.body;
+  const { username, email, password, dob } = req.body;
   const existingUser = await User.findOne({
     $or: [{ email }, { username }],
   });
@@ -20,11 +21,11 @@ const registerUser = async (req, res, next) => {
     }
   }
   const hashPassword = await bcrypt.hash(password, 10);
-  await User.create({ username , email, password: hashPassword , dob });
+  await User.create({ username, email, password: hashPassword, dob });
   res.json({ message: "User Created successfully" });
 };
 
-const loginUser = async (req, res,next) => {
+const loginUser = async (req, res, next) => {
   const { emailOrUsername, password } = req.body;
   const user = await User.findOne({
     $or: [{ email: emailOrUsername }, { username: emailOrUsername }],
@@ -37,6 +38,7 @@ const loginUser = async (req, res,next) => {
     return next(new CustomError("Invalid credentials", 400));
   }
   const token = createToken(user._id, user.role, "1h");
+  const refreshToken = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
   const currUser = {
     _id: user._id,
     username: user.username,
@@ -45,7 +47,26 @@ const loginUser = async (req, res,next) => {
     profile: user.profile,
     bio: user.bio,
   };
-  res.json({ message: "Login successful", token , user: currUser });
+  res.json({ message: "Login successful", token, user: currUser });
+};
+
+const refreshingToken = async (req, res, next) => {
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    return next(new CustomError("No refresh token provided", 401));
+  }
+
+  try {
+    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+    const accessToken = createToken(decoded.id, decoded.role, "1h");
+    res.status(200).json({
+      message: "Token refreshed",
+      token: accessToken,
+    });
+  } catch (error) {
+    return next(new CustomError("Invalid or expired refresh token", 401));
+  }
 };
 
 export { registerUser, loginUser };
