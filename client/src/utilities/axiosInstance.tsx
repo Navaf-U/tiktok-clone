dfdba01dfa5d848589d248bb40310a2204b95170
navaf <navafsoft@gmail.com>
2025-01-16 17:29:03 +0530

    feat: update axios instance to use environment variable for base URL and add .env to .gitignore

--- client/src/utilities/axiosInstance.tsx
+++ client/src/utilities/axiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000" as string,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
