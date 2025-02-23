5426ede457b7ae9cb5fceb5ffc7da3a35834a5f8
navaf <navafsoft@gmail.com>
2025-02-23 21:16:46 +0530

    refactor(middlewares, models): improve code formatting and readability in error handling and schema definitions

--- server/models/notificationSchema.js
+++ server/models/notificationSchema.js
@@ -1,32 +1,34 @@
 import mongoose from "mongoose";
 
-const notificationSchema = new mongoose.Schema({
-    receiver :{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
-        required : true
+const notificationSchema = new mongoose.Schema(
+  {
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    sender :{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "User",
-        required : true
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    type :{
-        type : String,
-        enum : ["like","follow","comment","message"],
-        required : true
+    type: {
+      type: String,
+      enum: ["like", "follow", "comment", "message"],
+      required: true,
     },
-    read :{
-        type : Boolean,
-        default : false
+    read: {
+      type: Boolean,
+      default: false,
     },
-    media :{
-        type : String,
-        default : "null",
-        required : false
-    }
+    media: {
+      type: String,
+      default: "null",
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps : true});
-
-const Notification = mongoose.model("Notification",notificationSchema);
-export default Notification; 
+const Notification = mongoose.model("Notification", notificationSchema);
+export default Notification;
