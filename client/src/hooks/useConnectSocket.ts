c47260cf15b6c9b9f4953dfcf5e4f3a98851eb24
Navaf <navafsoft@gmail.com>
2025-04-13 18:06:37 +0530

    Update useConnectSocket.ts

--- client/src/hooks/useConnectSocket.ts
+++ client/src/hooks/useConnectSocket.ts
@@ -1,6 +1,7 @@
 import { UserContext } from "@/context/UserProvider"
 import { useContext, useEffect } from "react"
 import {io} from 'socket.io-client'
+
 export const socket = io(import.meta.env.VITE_API_URL as string)
 function useConnectSocket() {
   const userContext = useContext(UserContext)
