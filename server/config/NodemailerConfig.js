6feef70b35977518aa73d2fb5a0ccd0daed7b125
navaf <navafsoft@gmail.com>
2025-04-23 14:58:33 +0530

    refactor(NodemailerConfig): comment out nodemailer configuration code

--- server/config/NodemailerConfig.js
+++ server/config/NodemailerConfig.js
@@ -1,14 +1,14 @@
-import nodemailer from 'nodemailer'
-import dotenv from 'dotenv'
+// import nodemailer from 'nodemailer'
+// import dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 
-const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_ID,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    });
+// const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user: process.env.EMAIL_ID,
+//             pass: process.env.EMAIL_PASSWORD
+//         }
+//     });
 
-export default transporter
+// export default transporter
